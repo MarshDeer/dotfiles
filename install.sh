0bc2791d3fc792b33fb8e1d48ab22c2693161060
MarshDeer <marshdeer97@gmail.com>
2022-04-18 14:44:24 -0400

    fixed install script

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ GREEN="\033[38;5;10m"
 
 # XKB
 	sudo -s rm /usr/share/X11/xkb/symbols/us
-	ln xkb/us /usr/share/X11/xkb/symbols/
+	ln ./xkb/us /usr/share/X11/xkb/symbols/
 	echo -e "${GREEN}XKB layout hardlinked succesfully${NO}"
 	echo -e "${RED}Remember to set your XKB layout to US-Dvorak!${NO}
 	"
@@ -20,7 +20,7 @@ GREEN="\033[38;5;10m"
 # Zathura
 	if [ -x /usr/bin/zathura ] ;
 		then
-			ln zathura/zathurarc /home/lena/.config/zathura/zathurarc
+			ln ./zathura/zathurarc /home/lena/.config/zathura/zathurarc
 			echo -e "${GREEN}zathurarc hardlinked succesfully${NO}"
 		else	
 			echo -e "${RED}zathura is not installed in this computer; skipping${NO}
@@ -29,15 +29,15 @@ GREEN="\033[38;5;10m"
 
 # Vim
 	rm /home/lena/.vimrc
-	ln -s vim/vimrc /home/lena/.vimrc
+	ln -s ./vim/vimrc /home/lena/.vimrc
 	echo -e "${GREEN}.vimrc softlinked successfully${NO}
 	"
 
 # Bash
 	rm /home/lena/.bashrc
 	rm /home/lena/.bash_aliases
-	ln -s bash/bashrc /home/lena/.bashrc
-	ln -s bash/bash_aliases /home/lena/.bash_aliases
+	ln -s ./bash/bashrc /home/lena/.bashrc
+	ln -s ./bash/bash_aliases /home/lena/.bash_aliases
 	source /home/lena/.bashrc
 	echo -e "${GREEN}.bashrc and .bash_aliases softlinked and sourced succesfully${NO}
 	"
