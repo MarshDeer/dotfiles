fa832202a202e58b8afb4a324959f38235eea766
MarshDeer <marshdeer97@gmail.com>
2023-03-13 20:46:29 -0300

    added autostart script

---
+++ autostart.sh
@@ -0,0 +1,7 @@
+xmodmap -e 'clear Lock'
+export GTK_USE_PORTAL=1
+
+xsetwacom set "Wacom Bamboo 16FG 4x5 Pad pad" button 1 key ctrl alt s
+xsetwacom set "Wacom Bamboo 16FG 4x5 Pad pad" button 9 key ctrl z
+xsetwacom set "Wacom Bamboo 16FG 4x5 Pad pad" button 8 key ctrl shift z
+xsetwacom set "Wacom Bamboo 16FG 4x5 Pad pad" button 3 key +space
