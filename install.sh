697ab5609ac28affbaf711777bcba4949cb2d1fe
lensdeer <102762572+MarshDeer@users.noreply.github.com>
2026-06-28 14:24:37 -0400

    install script: fixed xkb link handling; improved working directory issues etc

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $(dirname "$0")
+
 if [ ! command -v dialog ] || [ ! command -v stow ]; then
 	echo "ERROR - Install script requires 'dialog' and 'stow' to be available in \$PATH"
 	exit 2
@@ -6,7 +9,7 @@ fi
 
 choices=$(dialog --stdout --no-items --keep-window --checklist \
 	"Select the dotfiles to be stowed in $HOME" \
-	25 45 15\
+	25 45 15 \
 	$(dir -d -1 */ | sed '/^xkb\/$/d' | tr -d / | awk '{print $1, "off"}')
 )
 
@@ -19,7 +22,6 @@ for package in $choices; do
 done
 
 if dialog --yesno "Do you want to add Spanish characters to the us(dvorak) keyboard layout?" 6 50; then
-	sudo cp /usr/share/X11/xkb/symbols/us /usr/share/X11/xkb/symbols/us.bak
-	sudo ln -sf xkb/us /usr/share/X11/xkb/symbols/us
-	dialog --msgbox "Done! Old xkb symbols definition backed up in /usr/share/X11/xkb/symbols/us.bak" 6 50
+	sudo ln -sfb $(pwd)/xkb/us /usr/share/X11/xkb/symbols/us
+	dialog --msgbox "Done! Old xkb symbols definition backed up in '/usr/share/X11/xkb/symbols/us~'" 6 50
 fi
